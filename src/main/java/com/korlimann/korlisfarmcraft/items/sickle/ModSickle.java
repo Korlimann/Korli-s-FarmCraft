74e2662db3ec7a567cf39b476c4b34fbdb6a3b95
theOnlyHorst <buschsteffmichi@chello.at>
2019-08-19 16:42:36 +0200

    Rewrote Sickles and Scythes to not act as hoe's

--- src/main/java/com/korlimann/korlisfarmcraft/items/sickle/ModSickle.java
+++ src/main/java/com/korlimann/korlisfarmcraft/items/sickle/ModSickle.java
@@ -6,6 +6,7 @@ import com.korlimann.korlisfarmcraft.setup.ModSetup;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.CropsBlock;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
 import net.minecraft.entity.player.PlayerEntity;
@@ -16,7 +17,7 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-public class ModSickle extends HoeItem {
+public class ModSickle extends TieredItem {
     private final float attackDamage;
     private final float attackSpeed;
 
@@ -51,7 +52,7 @@ public class ModSickle extends HoeItem {
             public Ingredient getRepairMaterial() {
                 return repairMaterial;
             }
-        }, attackSpeed*-1, new Properties().maxStackSize(1).group(ModSetup.itemGroup));
+        }, new Properties().maxStackSize(1).group(ModSetup.itemGroup));
         setRegistryName(registryName);
         this.attackSpeed = attackSpeed;
         this.attackDamage = attackDamage-1f;
@@ -69,6 +70,9 @@ public class ModSickle extends HoeItem {
             if(random.nextInt(100)<59) {
                 player.inventory.addItemStackToInventory(new ItemStack(Items.WHEAT,1));
             }
+            context.getItem().damageItem(1, player, (p_220042_0_) -> {
+                p_220042_0_.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+            });
         }
         return ActionResultType.SUCCESS;
     }
@@ -80,5 +84,11 @@ public class ModSickle extends HoeItem {
             multimap.put(SharedMonsterAttributes.ATTACK_SPEED.getName(), new AttributeModifier(ATTACK_SPEED_MODIFIER, "Weapon modifier", (double)this.attackSpeed, AttributeModifier.Operation.ADDITION));
         }
         return multimap;
+    }
+    public boolean hitEntity(ItemStack stack, LivingEntity target, LivingEntity attacker) {
+        stack.damageItem(1, attacker, (p_220042_0_) -> {
+            p_220042_0_.sendBreakAnimation(EquipmentSlotType.MAINHAND);
+        });
+        return true;
     }
 }
